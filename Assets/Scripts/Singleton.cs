f616674b783f654d8ae5daa0375a855686165cfe
Nicolas STROHL <strohl@c-koya.tech>
2023-09-08 23:25:20 +0200

    Ajout GameManager en Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject singletonGO = new GameObject(typeof(T).Name);
+                    _instance = singletonGO.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
