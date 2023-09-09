0b44d7d10cee744b45306aaf4c1cdef758b4cfe5
Nicolas STROHL <strohl@c-koya.tech>
2023-09-09 21:07:11 +0200

    Possibilité de tabasser les ennemis et ils perdent de la vie

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private int maxHP = 20;
+    private int currentHp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHp = maxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(int damage)
+    {
+        this.currentHp -= damage;
+        if (this.currentHp <= 0) Destroy(gameObject); 
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour
     public int XCoordinate = 0;
     public int YCoordinate = 0;
     private bool isHoldingKey = false;
+    public int attack = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,36 +28,45 @@ public class Player : MonoBehaviour
 
     void HandleMovement()
     {
-        //if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) + Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0) 
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0f)
             {
+                if (isHoldingKey) return;
+                isHoldingKey = true;
                 RaycastHit2D hit = Physics2D.Linecast(new Vector2(movePoint.position.x, movePoint.position.y), new Vector2(movePoint.position.x + Input.GetAxisRaw("Horizontal"), movePoint.position.y));
                 if (hit.transform)
                 {
                     if (hit.transform.gameObject.tag == "Enemy")
                     {
-                        //TODO : Frapper fort
+                        hit.transform.gameObject.GetComponent<Enemy>().TakeDamage(attack);
+                        GameManager.Instance.StartNextTurnAndPerformSideEffects();
                     }
                     return;
                 }
 
-                if (isHoldingKey) return;
                 movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
                 GameManager.Instance.StartNextTurnAndPerformSideEffects();
-                isHoldingKey = true;
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0f)
             {
-                RaycastHit2D hit = Physics2D.Linecast(new Vector2(movePoint.position.x, movePoint.position.y), new Vector2(movePoint.position.x, movePoint.position.y + Input.GetAxisRaw("Vertical")));
-                if (hit.transform) return;
                 if (isHoldingKey) return;
+                isHoldingKey = true;
+                RaycastHit2D hit = Physics2D.Linecast(new Vector2(movePoint.position.x, movePoint.position.y), new Vector2(movePoint.position.x, movePoint.position.y + Input.GetAxisRaw("Vertical")));
+                if (hit.transform)
+                {
+                    if (hit.transform.gameObject.tag == "Enemy")
+                    {
+                        hit.transform.gameObject.GetComponent<Enemy>().TakeDamage(attack);
+                        GameManager.Instance.StartNextTurnAndPerformSideEffects();
+                    }
+                    return;
+                }
+
                 movePoint.position += new Vector3(0, Input.GetAxisRaw("Vertical"), 0);
                 GameManager.Instance.StartNextTurnAndPerformSideEffects();
-                isHoldingKey = true;
             }
             else
             {
